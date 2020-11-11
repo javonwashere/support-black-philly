50037f6e0f62ddc10ab85924a35e1d225f04c84d
javonwashere <javonmcgilberry@gmail.com>
2020-11-11 01:04:30 -0500

    move style sheets

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import "./src/index.css";
 import "./src/components/base.css";
+import "./src/components/header/header.css";
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "gatsby";
-import styles from "./header.module.css";
 import fullLogo from "../../assets/sbp_full_logo.png";
 import monogram from "../../assets/sbp_monogram.png";
 
@@ -10,30 +9,17 @@ import instagramIcon from "../../assets/socials/instagram.svg";
 import twitterIcon from "../../assets/socials/twitter.svg";
 import everybodyBlack from "../../assets/everybody_black.png";
 
-// export default () => (
-//   <nav role="navigation">
-//     <ul className={styles.navigation}>
-//       <li className={styles.navigationItem}>
-//         <Link to="/">Home</Link>
-//       </li>
-//       <li className={styles.navigationItem}>
-//         <Link to="/blog/">Blog</Link>
-//       </li>
-//     </ul>
-//   </nav>
-// )
-
 export default ({ data }) => {
   const { twitter, facebook, instagram } = data;
   console.log(data);
   return (
-    <Navbar className="navbar">
+    <div class="navbar">
       <NavWrapper className="header-animation">
-        <ImgWrapper>
-          <MonoLogoContainer src={monogram} />
+        <ImgWrapper className="header-img-wrapper">
+          <MonoLogoContainer className="mono-logo" src={monogram} />
         </ImgWrapper>
-        <ImgWrapper>
-          <TextLogoContainer src={fullLogo} />
+        <ImgWrapper className="header-img-wrapper">
+          <TextLogoContainer className="text-logo" src={fullLogo} />
         </ImgWrapper>
         <SocialIconsContainer className="social-icons">
           <a href={instagram}>
@@ -50,66 +36,34 @@ export default ({ data }) => {
           <img src={everybodyBlack} />
         </div>
       </NavWrapper>
-    </Navbar>
+    </div>
   );
 };
 
 const ImgWrapper = styled.div`
-  width: 100%;
+  // width: 100%;
 `;
 
 const SocialIconsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  justify-content: flex-end;
+
 `;
 
 const SocialIcon = styled.img`
-  height: 1px;
-  height: 20px;
-  width: 10px;
-  width: auto !important;
-  max-width: 10vw !important;
-  padding-left: 1vw;
 `;
 
 const Navbar = styled.div`
-  position: fixed;
-  top: 0;
-  z-index: 5;
-  align-items: center;
-  display: flex;
-  width: 100%;
-  height: 20vh;
-  background-color: rgba(251, 248, 234, 1);
-  //   padding: 3vh 2vw;
+  
 `;
 
 const NavWrapper = styled.div`
-  display: flex;
-  max-width: 1180px;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  // position: relative;
+  
 `;
 
 const MonoLogoContainer = styled.img`
-  height: 1px;
-  height: 10vw;
-  max-height: 10vh;
-  width: auto !important;
-  max-width: 10vw !important;
 `;
 
 const TextLogoContainer = styled.img`
-  height: 1px;
-  height: 18vw;
-  max-height: 18vh;
-  width: auto !important;
+
 `;
 
 const LogoContainer = styled.img`
